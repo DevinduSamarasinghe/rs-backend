200c3271fcb2caf934cc0bc6f17021a31aeb87ba
DevinduSamarasinghe <devindu990@gmail.com>
2023-07-24 14:20:26 +0530

    feat: create controllers for users, messages and chats

---
+++ controllers/messageController.ts
@@ -0,0 +1,44 @@
+import User from "../models/user.model";
+import Chat from "../models/chatModel";
+import Message from "../models/messageModel";
+import { IRequest } from "../types/Request";
+
+export const sendMessage = async(req:IRequest, res:any)=>{
+    const {content,chatId} = req.body;
+
+    if(!content || !chatId){
+        return res.sendStatus(400).send("Invalid data passed into request");
+    }
+
+    var newMessage = {
+        sender: req.user!._id,
+        content: content,
+        chat: chatId
+    };
+
+    try{
+        var message:any = await Message.create(newMessage);
+        message = await message.populate("sender", "-password");
+        message = await message.populate("chat");
+        message = await User.populate(message, {path: "chat.users", select: "-password"});
+        message = await Message.populate(message, {path: "chat.latestMessage",select:"content sender"});
+        message = await User.populate(message, {path: "chat.latestMessage.sender", select: "firstName lastName email"})
+
+        await Chat.findByIdAndUpdate(chatId, {latestMessage: message});
+        res.json(message);
+        
+    }catch(error:any){
+        res.status(400).json("Failed to send message", error.message)
+    }   
+}
+
+export const allMessages = async(req:IRequest, res:any)=>{
+
+    const {chatId} = req.body;
+    try{
+        const messages = await Message.find({chat: chatId}).populate('sender','firstName lastName pic email');
+        res.status(200).json(messages);
+    }catch(error:any){
+        res.status(400).json("Failed to fetch messages", error.message);
+    }
+}
