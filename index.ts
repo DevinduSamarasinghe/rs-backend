ff405fa5f919b6e06db9a1239435e007220acd52
DevinduSamarasinghe <devindu990@gmail.com>
2023-08-18 07:09:59 +0530

    feat: socket.io integration

--- index.ts
+++ index.ts
@@ -7,16 +7,16 @@ import MessageRouter from "./routes/messages.routes";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { deserealizeUser } from "./middleware/deserialization";
+import { Server } from "socket.io";
 dotenv.config({ path: ".env" });
 
 const app: Express = express();
 const PORT = 8080 || process.env.PORT;
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL  || "*",
+  origin: process.env.FRONTEND_URL || "*",
   credentials: true,
 };
-
 
 (function main() {
   app.use(express.json());
@@ -36,7 +36,46 @@ const corsOptions = {
   connectDb();
 
   //server rendering
-  app.listen(PORT, () => {
+  const server = app.listen(PORT, () => {
     console.log(`Server Running on PORT: ${PORT}`);
+  });
+
+  //socket.io server configuration
+  const io = new Server(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: process.env.FRONTEND_URL,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Connected to Socket.io");
+
+    socket.on("setup", (userData) => {
+      console.log("UserData:", userData);
+      socket.join(userData._id);
+      socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User joined room: ", room);
+    });
+
+    //message returns
+    socket.on("new message", (newMessageReceived) => {
+      var chat = newMessageReceived.chat;
+      console.log("Chat is: ", newMessageReceived.chat);
+
+      if (!chat.users) return console.log("Chat.users not defined");
+
+      //Type of user
+      chat.users.forEach((user: any) => {
+        if (user._id === newMessageReceived.sender._id) return;
+        console.log(user._id);
+        socket.in(user._id).emit("Message received", newMessageReceived);
+        console.log("Message sent to: ", user._id);
+      });
+    });
   });
 })();
